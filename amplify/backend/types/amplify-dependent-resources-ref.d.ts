600b9b6ac1b341760f79aa6327783e66c52e46cc
NirK8 <nir@moveohls.com>
2021-11-19 23:50:27 +0200

    removed auth 3

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,13 +1,1 @@
-export type AmplifyDependentResourcesAttributes = {
-    "auth": {
-        "githubdemo0e5f06f8": {
-            "IdentityPoolId": "string",
-            "IdentityPoolName": "string",
-            "UserPoolId": "string",
-            "UserPoolArn": "string",
-            "UserPoolName": "string",
-            "AppClientIDWeb": "string",
-            "AppClientID": "string"
-        }
-    }
-}
+export type AmplifyDependentResourcesAttributes = {}
--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <AmplifySignOut />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Nir Kahana Demo App</p>
@@ -20,4 +23,4 @@ function App() {
   );
 }
 
-export default App;
+export default withAuthenticator(App);
