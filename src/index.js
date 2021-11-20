392c99756919abe9516f219c4f360408b819c0ec
NirK8 <nir@moveohls.com>
2021-11-20 17:17:02 +0200

    removed unnecessary code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { AmplifyProvider } from "@aws-amplify/ui-react";
 
 import "./index.css";
 import App from "./App";
@@ -8,9 +7,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AmplifyProvider>
-      <App />
-    </AmplifyProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
