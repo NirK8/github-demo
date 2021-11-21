7398291acb3a626a7db1cdc40671ba0ac1b2be64
NirK8 <nir@moveohls.com>
2021-11-21 12:36:39 +0200

    added storage

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,8 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "storage": {
+        "s3githubdemostoragedaa4n7nm7zl8o": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
